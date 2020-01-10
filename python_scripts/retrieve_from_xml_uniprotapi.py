742111379c4cb8897d7a8f0c12092c0b0f83eab1
daniel <daniel@maquina>
2020-01-09 18:46:59 -0600

    modif retrieve...py

--- python_scripts/retrieve_from_xml_uniprotapi.py
+++ python_scripts/retrieve_from_xml_uniprotapi.py
@@ -18,100 +18,64 @@ def get_uniprot_data(accession):
     return bs_data
 
 
-def get_accession(bs_data):
-    ''' get accession number from bs_data from get_uniprot_data'''
-    return bs_data.gnentries.gnentry.accession.text
+
+def get_gralinfo(bs_data):
+    ''' get general info from bs_data from get_uniprot_data
+        uniprot_id, full_prot_name,reverse, chromosome, start_gene_coord, genom_end_coord, sequence'''
+    sequence =  bs_data.gnentries.gnentry.sequence.text # sequence
+    uniprot_id = bs_data.gnentries.gnentry.accession.text # uniprot accession 
+    full_prot_name = bs_data.gnentries.gnentry.protein.recommendedname.fullname.text # full prot name
+    reverse = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['reverse_strand'] # rev strand
+
+    chromosome = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['chromosome'] #chromosome
+    start_gene_coord = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['start'] #start
+    genom_end_coord = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['end'] #ends
+
+    text_list = [uniprot_id, full_prot_name,reverse, chromosome, start_gene_coord, genom_end_coord, sequence]
+    text_output = '|'.join(text_list)
 
 
-def get_sequence(bs_data):
-    ''' get sequence from bs_data from get_uniprot_data'''
-    return bs_data.gnentries.gnentry.sequence.text
-
-def get_full_prot_name(bs_data):
-    ''' get full protein name from bs_data from get_uniprot_data'''
-    return bs_data.gnentries.gnentry.protein.recommendedname.fullname.text
-
-
+# need to extract short name, some do not have short name     
 def get_alternative_prot_names(bs_data):
     ''' get alternative prot name from bs_data from get_uniprot_data,
         returns a list'''
-    alternative_names = [] # null list
-
+    uniprot_id = bs_data.gnentries.gnentry.accession.text
     # get list of all alternative names
     all_alternatives = bs_data.gnentries.gnentry.protein.findAll('alternativename')
-
+    tmp_alternatives = [] # null list
     # append to list
     for alternative in all_alternatives:
-        alternative_names.append(alternative.text)
-    return alternative_names
-    # should return a list
-
-def get_rev(bs_data):
-    ''' get if it is on the reverse strand from bs_data from get_uniprot_data,
-        takes the first sequence as reference and extracts info from there
-        returns a True/False'''
-    reverse = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['reverse_strand']) # rev strand
-    return reverse
+        text_list = [uniprot_id, alternative.text]
+        text_output = '|'.join(text_list)
+        tmp_alternatives.append(text_output)
+    return tmp_alternatives
+    
+#<alternativeName><fullName>Protein kinase B alpha</fullName><shortName>PKB alpha</shortName></alternativeName><alternativeName><fullName>Protein kinase B</fullName><shortName>PKB</shortName>
 
 
-def get_chrom(bs_data):
-    ''' get chromosome from bs_data from get_uniprot_data,
-        takes the first sequence as reference and extracts info from there
-        returns a number'''
-    chromosome = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['chromosome']) #chromosome
-    return chromosome
-
-
-def get_genm_coord_start(bs_data):
-    ''' get gene genom coordinate from bs_data from get_uniprot_data,
-        takes the first sequence as reference and extracts info from there
-        returns a number'''
-    start_gene_coord = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['start']) #start
-    return start_gene_coord
-
-
-def get_genm_coord_end(bs_data):
-    ''' get gene genom coordinate from bs_data from get_uniprot_data,
-        takes the first sequence as reference and extracts info from there
-        returns a number'''
-    genom_end_coord = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].genomiclocation['end']) #ends
-    return genom_end_coord
 
 
 
 ### bellow are lists
-
 def get_ensembl_geneid(bs_data):
     ''' get ensembl gene id name from bs_data from get_uniprot_data,
         returns a list'''
+    uniprot_id = bs_data.gnentries.gnentry.accession.text # uniprot accession 
     genes_ids = []
     genes = bs_data.gnentries.gnentry.findAll('gncoordinate')
     for gene in genes:
-        gene_ids.append(gene['ensembl_gene_id'] )
+        ensembl_gene_id = gene['ensembl_gene_id']
+        ensembl_transcript_id = gene['ensembl_transcript_id']
+        ensembl_translation_id = gene['ensembl_translation_id']
+        print(ensembl_gene_id)
+        text_list = [uniprot_id, ensembl_gene_id, ensembl_transcript_id, ensembl_translation_id]
+        text_output = '|'.join(text_list)
+        genes_ids.append(text_output)
+
     return genes_ids
-    # should return a list
 
 
-def get_ensembl_transcid(bs_data):
-    ''' get ensembl transcript id name from bs_data from get_uniprot_data,
-        returns a list'''
-    genes = bs_data.gnentries.gnentry.findAll('gncoordinate')
-    gene_transcids = []
-    for gene in genes:
-        gene_transcids.append(gene['ensembl_transcript_id'])
-    return gene_transcids
-    # should return a list
 
-
-def get_ensembl_tranlid(bs_data):
-    ''' get ensembl translation id from bs_data from get_uniprot_data,
-        returns a list'''
-    genes = bs_data.gnentries.gnentry.findAll('gncoordinate')
-    gene_translids = []
-    for gene in genes:
-        gene_translids.append(gene['ensembl_translation_id'])
-    return gene_translids
-    # should return a list
 
 
 ## get modified residues
@@ -123,11 +87,60 @@ def data_modif_res(bs_data):
     phospho_list = []
     for entry in features:
         if entry['type'] == 'modified residue': # extract modified residues
-             if 'hospho' in entry.findAll('ns2:description')[0].text: # is phosphosomething ?
+             if 'hospho' in entry.findAll('ns2:description')[0].text: # is phospho...something ?
                  phospho_list.append(entry)
     return phospho_list
 
 
+
+def modres_gral_info(bs_data):
+    ''' General information of Modified residues: position, type, genom_begin, genom_end.
+       Columns separated by "|"
+       Returns a list. '''
+    mod_res_list = data_modif_res(bs_data)
+    uniprot_id = bs_data.gnentries.gnentry.accession.text
+    tmp_list = []
+    
+    for mod_res_item in mod_res_list:
+        
+        residue_position = str(mod_res_item.findAll('ns2:position')[0]['position']) # position
+        type_modif = mod_res_item.findAll('ns2:description')[0].text # type of modif
+        
+        genom_begin = str(mod_res_item.findAll('ns2:begin')[0]['position']) #genom_beg_pos
+        genom_end = str(mod_res_item.findAll('ns2:end')[0]['position']) #genom_end_pos
+        
+        text_list = [uniprot_id, residue_position, type_modif, genom_begin, genom_end]
+        text_row = '|'.join(text_list)
+        
+        tmp_list.append(text_row)
+        
+    return tmp_list
+
+
+
+def modres_references(bs_data):
+    ''' References of Modified residues: residue_position, id,type 
+       Columns separated by "|"
+       Returns a list. '''
+    uniprot_id = bs_data.gnentries.gnentry.accession.text
+    mod_res_list = data_modif_res(bs_data)
+    tmp_main_list = []
+    
+    for mod_res_item in mod_res_list:
+        residue_position = mod_res_item.findAll('ns2:position')[0]['position'] # position
+        
+        references = mod_res_item.findAll('ns2:dbreference') # position of modif residue
+        tmp_ref_list = []
+        for reference in references:
+            tmp_tup = (reference['id'],reference['type']) # (ref_id,ref_type)
+            tmp_ref_list.append(tmp_tup)
+        
+        for item in tmp_ref_list:
+            text_list = [uniprot_id, residue_position, item[0], item[1]]
+            text_output = '|'.join(text_list)
+            tmp_main_list.append(text_output)
+    
+    return tmp_main_list
 
 #data_modif_res = bs_data.gnentries.gnentry.findAll('gncoordinate')[0].findAll('feature')
 #for entry in data_modif_res:
