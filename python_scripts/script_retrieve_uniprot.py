63f0ec5e6d14bb912010f17ec57adb3175c21e52
nobeeakon <dtg30@ioc025.student.eecs.qmul.ac.uk>
2020-01-13 11:54:07 +0000

    modif script retrieve uniprot

--- python_scripts/script_retrieve_uniprot.py
+++ python_scripts/script_retrieve_uniprot.py
@@ -15,36 +15,118 @@ OUT_ENSEMBL = open(csvEnsembl,'w')
 OUT_MODRESGRAL = open(csvModResGral,'w')
 OUT_MODRESREFS = open(csvModResRefs,'w')
 
-OUT_GRALINFO.write('uniprot_id|full_prot_name|reverse|chromosome|start_gene_coord|genom_end_coord|sequence')
-OUT_ALTPROT.write('uniprot_id|name|short')
-OUT_ENSEMBL.write('uniprot_id|ensembl_gene_id|ensembl_transcript_id|ensembl_translation_id')
-OUT_MODRESGRAL.write('uniprot_id|residue_position|type_modif|genom_begin|genom_end')
-OUT_MODRESREFS.write('uniprot_id|residue_position|ref_id|ref_type')
+
+# writing headers on output files
+OUT_GRALINFO.write('uniprot_id|full_prot_name|reverse|chromosome|start_gene_coord|genom_end_coord|sequence\n')
+OUT_ALTPROT.write('uniprot_id|name|short\n')
+OUT_ENSEMBL.write('uniprot_id|ensembl_gene_id|ensembl_transcript_id|ensembl_translation_id\n')
+OUT_MODRESGRAL.write('uniprot_id|residue_position|type_modif|genom_begin|genom_end\n')
+OUT_MODRESREFS.write('uniprot_id|residue_position|ref_id|ref_type\n')
+
+
+# read kinase list
+
+# read headers
+print(INFILE.readline())
+# we want uniprot  [3]
+#Family,name,name_human,uniprot,mass
+#AGC_Ser/Thr,AKT1,AKT1_HUMAN,P31749,55.686
+
+# errors lists
+## --  gral info
+error_list_get_gralinfo = []
+## -- alternative names
+error_get_alternative_prot_names = []
+## -- ensembl ids 
+error_list_get_ensembl_geneid = []
+## -- modif residue information
+error_modres_references = []
+
+counter = 1
+
+for line in INFILE:
+    line = line.rstrip()
+    tmp_list = line.split(',')
+    uniprot = tmp_list[3]
+    print('------------------------------------------------------')
+    print('----------------')
+    print(counter)
+    print(uniprot)
+    print('----------------')
+    ## get data
+    data = xmluniprot.get_uniprot_data(uniprot)
+    ## --  gral info
+    try:
+        gralinfo = xmluniprot.get_gralinfo(data)
+        out = gralinfo + '\n'
+        OUT_GRALINFO.write(out) # write 
+    except:
+        error_list_get_gralinfo.append(uniprot)
+        print('!! some info not found: get_gralinfo: '+ uniprot)
+    ## -- alternative names
+    try:
+        alt_prot = xmluniprot.get_alternative_prot_names(data)
+        if (len(alt_prot) > 1):
+            for entry in alt_prot:
+                out = entry + '\n'
+                OUT_ALTPROT.write(out) #write 
+        else:
+            out = ''.join(alt_prot) + '\n'
+            OUT_ALTPROT.write(out) #write
+    except:
+        error_get_alternative_prot_names.append(uniprot)
+        print('!! some info not found: get_alternative_prot_names: '+ uniprot)
+    ## -- ensembl ids 
+    try: 
+        ensembl_id = xmluniprot.get_ensembl_geneid(data)
+        if(len(ensembl_id) > 1):
+            for entry in ensembl_id:
+                out = entry + '\n'
+                OUT_ENSEMBL.write(out) #write
+        else:
+            out = ''.join(ensembl_id) + '\n'
+            OUT_ENSEMBL.write(out) # write
+    except:
+        error_list_get_ensembl_geneid.append(uniprot)
+        print('!! some info not found: get_ensembl_geneid:'+ uniprot)
+    ## -- modif residue information
+    try:
+        modifres_gralinfo = xmluniprot.modres_gral_info(data)
+        for entry in modifres_gralinfo:
+            out = entry + '\n'    
+            OUT_MODRESGRAL.write(out) #write
+        ## modified residues references
+        modres_refs = xmluniprot.modres_references(data)
+        if(len(modres_refs) > 1 ): 
+            for entry in modres_refs:
+                out = entry + '\n'
+                OUT_MODRESREFS.write(out) #write
+        else: 
+            out = ''.join(modres_refs) + '\n'
+            OUT_MODRESREFS.write(out) #write
+    except:
+        error_modres_references.append(uniprot)
+        print('!! some info not found: modres_references: '+ uniprot)
+    print('------------------------------------------------------')
+    counter += 1
+
+
+OUT_GRALINFO.close()
+OUT_ALTPROT.close()
+OUT_ENSEMBL.close()
+OUT_MODRESGRAL.close()
+OUT_MODRESREFS.close()
+
+INFILE.close()
 
 
 
-
-# OUT_GRALINFO = open(,'w')
-
-
-from
-
-uniprot = 'P31749'
-
-data = xmluniprot.get_uniprot_data(uniprot)
-
-gralinfo = xmluniprot.get_gralinfo(data)
-for entry in gralinfo:
-    OUT_GRALINFO.
-
-alt_prot = xmluniprot.get_alternative_prot_names(data)
-alt_prot
-
-ensembl_id = xmluniprot.get_ensembl_geneid(data)
-ensembl_id
-
-modifres_gralinfo = xmluniprot.modres_gral_info(data)
-modifres_gralinfo
-
-modres_refs = xmluniprot.modres_references(data)
-modres_refs
+# errors lists
+## --  gral info
+error_list_get_gralinfo 
+## -- alternative names
+error_get_alternative_prot_names 
+## -- ensembl ids 
+error_list_get_ensembl_geneid 
+## -- modif residue information
+error_modres_references
