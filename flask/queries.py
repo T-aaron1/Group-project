fc17d8f2f0b01379417af01c092519df58b4a75a
daniel <daniel@maquina>
2020-01-27 07:49:12 -0600

    information of particular kinase now displays info

--- flask/queries.py
+++ flask/queries.py
@@ -3,19 +3,21 @@
 import sqlite3
 import pandas as pd
 
-def select_gral(DATABASE, select_text,from_table, where_data, like_something):
-    query = "SELECT {} FROM {} WHERE {} LIKE '{}'".format(select_text,from_table, where_data, like_something)
+def select_gral(DATABASE, select_text,from_table, where_data):
+    query = "SELECT {} FROM {} WHERE {}".format(select_text,from_table, where_data)
     db = sqlite3.connect(DATABASE)
     db_results = pd.read_sql_query(query, db)
     db.close()
     return  db_results
 
-def query_n_results(DATABASE, select_text,from_table, where_data, like_something):
-    db_results = select_gral(DATABASE, select_text,from_table, where_data, like_something)
-    n_results = db_results.shape[0]
+def query_n_results(DATABASE, select_text,from_table, where_data):
+    query = "SELECT COUNT({}) FROM {} WHERE {}".format(select_text,from_table, where_data)
+    db = sqlite3.connect(DATABASE)
+    n_results = pd.read_sql_query(query, db).iloc[0,0]
+    db.close()
     return  n_results
 
 
-def query_is_unique(DATABASE, select_text,from_table, where_data, like_something):
-    n_results = query_n_results(DATABASE, select_text,from_table, where_data, like_something)
+def query_is_unique(DATABASE, select_text,from_table, where_data):
+    n_results = query_n_results(DATABASE, select_text,from_table, where_data)
     return  n_results == 1
