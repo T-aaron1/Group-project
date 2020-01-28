f67945b9aa39e62293fcf5b45246bdf4cf647fe6
nobeeakon <dtg30@ioc046.student.eecs.qmul.ac.uk>
2020-01-28 18:49:06 +0000

    modified home html, forms and kinase_data html

--- flask/forms.py
+++ flask/forms.py
@@ -1,26 +1,32 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError
 
-from wtforms.fields import IntegerField
+def my_length_check(form, field):
+    if len(field.data) < 2:
+        raise ValidationError('Search string must be larger than 1 character')
+
+def is_alpha_numeric(form, field):
+    if not field.data.isalnum():
+        raise ValidationError('Non alphanumeric characters are not allowed')
+
 
 class Search_string(FlaskForm):
-    search_string = StringField('', validators=[DataRequired()])
-
+    search_string = StringField('', validators=[DataRequired(), my_length_check, is_alpha_numeric ])
 
 
 class Inhibitor_used(FlaskForm):
     inhibitor = StringField('Used Inhibitor', validators=[DataRequired()])
 
+
 class Threshold_pval(FlaskForm):
-     threshold_pval = IntegerField('P-value Threshold', validators=[DataRequired()])
+     threshold_pval = IntegerField('P-value Threshold')
 
 class Threshold_foldchange(FlaskForm):
-     threshold_foldchange = IntegerField('Foldchange Threshold', validators=[DataRequired()])
+     threshold_foldchange = IntegerField('Foldchange Threshold')
 
 
 class UploadForm(FlaskForm):
-    uploaded_file = FileField('', validators=[
-        FileAllowed(['tsv'], 'Should be ".tsv"')
-    ])
+    uploaded_file = FileField('', validators=[FileAllowed(['tsv'], 'Should be ".tsv"')])
