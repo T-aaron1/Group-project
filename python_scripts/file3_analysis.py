75a2cc28121706adf4c47c38b328e08d8ad29130
T-aaron1 <59611673+T-aaron1@users.noreply.github.com>
2020-02-07 18:28:06 +0000

    Add files via upload
    early python script for file 3 analysis

---
+++ python_scripts/file3_analysis.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[65]:
+
+
+import pandas as pd
+import numpy as np
+import re
+
+#file path
+df=pd.read_csv(r'C:\Users\Timot\Downloads\mux.tsv',sep='\t')
+
+df.columns = map(str.lower, df.columns)
+
+#drop rows and columns with no value
+df.dropna(axis=1, how='all', inplace=True)
+df.dropna(axis=0, how='all', inplace=True)
+#substrate  control_mean          mean  fold_change   p-value    ctrlCV   treatCV
+
+
+
+#split substrate column into name and position 
+df['subst_position'] = ''
+df['subst_position'] = df.substrate.str.split("(", n=1, expand=True)[1].str.replace(")", "")
+df['subst_name'] = df.substrate.str.split("(", n=1, expand=True)[0]
+
+#creating a new dataframe with control_mean, substrate position and substrate name
+
+
+control_subst = df[['subst_name','subst_position','control_mean']]
+df.drop(['subst_name','subst_position','control_mean'], axis=1, inplace=True)
+
+
+
+#extracting unique inhibitor name 
+inhibitors = []
+for name in df.columns:
+        if ('fold_change' in name):
+                inhibitor = name.rsplit('_')[0]
+                inhibitor = inhibitor.lower().rstrip()
+                inhibitors.append(inhibitor)
+print (inhibitors)
+
+
+
+# In[ ]:
+
+
+
+
