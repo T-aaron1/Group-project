46bd09a0b1c5c993f8f2b10ba4ba79437252c85d
daniel <daniel@maquina>
2020-02-12 11:50:16 -0600

    improved phosphoprot

--- flask/app/phosphoproteomics.py
+++ flask/app/phosphoproteomics.py
@@ -39,39 +39,56 @@ def phosphoproteomics():
         cv_treatment_threshold = request.values['cvt']
         tmp_file_path = os.path.join(phosphoproteomics_blueprint.config['UPLOAD_FOLDER'], tmp_file_name)
 
-#        try:
-        output = phosphoproteomics_script.file_handle(tmp_file_path)
-        substrate  = output['substrate']
+        try:
+            # get the data from the database
+            query = "SELECT {} FROM {}".format('basick.prot_name AS kinase, basicsub.gene AS sub_gene, subs.sub_mod_rsd, basicsub.prot_name AS substrate', \
+                                               'kinase_substrate subs LEFT JOIN basic_info basick ON subs.kin_acc_id = basick.uniprot_id LEFT JOIN  basic_info basicsub ON subs.sub_acc_id = basicsub.uniprot_id')
+            db = sqlite3.connect(phosphoproteomics_blueprint.config['DATABASE'])
+            kin_substrate = pd.read_sql_query(query, db)
+            db.close()
 
-        query = "SELECT {} FROM {}".format('basick.prot_name AS kinase, basicsub.gene AS sub_gene, subs.sub_mod_rsd, basicsub.prot_name AS substrate', \
-                                           'kinase_substrate subs LEFT JOIN basic_info basick ON subs.kin_acc_id = basick.uniprot_id LEFT JOIN  basic_info basicsub ON subs.sub_acc_id = basicsub.uniprot_id')
-        db = sqlite3.connect(phosphoproteomics_blueprint.config['DATABASE'])
-        kin_substrate = pd.read_sql_query(query, db)
-        db.close()
+            output = phosphoproteomics_script.file_handle(tmp_file_path, kin_substrate)
+            substrate  = output['substrate']
 
-        results_dict = {}
-        for key in output['inhibitors_dict'].keys():
-            inhibitor = key
-            ddf = substrate.join(output['inhibitors_dict'].get(key))
-            results_volcano = phosphoproteomics_script.volcano(ddf,pval_threshold, fold_threshold,  cv_treatment_threshold)
-            df_volcano = phosphoproteomics_script.extract_above_threshold(ddf, results_volcano)
+        except:
+            os.remove(tmp_file_path)
+            return 'Something went wrong during initial file handling, please check that the file has the right format and column names <a href="/"> Go back </a>'
 
-            z_score = phosphoproteomics_script.KSEA(ddf, kin_substrate) # for all
+        try :
+            print(output['inhibitors_dict'].keys())
+            print('------------------ññ----')
+            results_dict = {}
+            for key in output['inhibitors_dict'].keys():
+                inhibitor = key
+                ddf = substrate.join(output['inhibitors_dict'].get(key))
+                results_volcano = phosphoproteomics_script.volcano(ddf,pval_threshold, fold_threshold,  cv_treatment_threshold)
+                df_volcano = phosphoproteomics_script.extract_above_threshold(ddf, results_volcano)
 
-            # just for the ones that are above threshold in volcano plot
-            z_score_volcano = phosphoproteomics_script.KSEA(df_volcano, kin_substrate)
-            results_dict[inhibitor] = {
-              'volcano': results_volcano, 'fold_threshold': fold_threshold,
-              'pval_threshold': pval_threshold, 'non_identified': z_score['non_identified'],
-              'z_score': z_score['score'], 'non_identified_volcano':z_score_volcano['non_identified'],
-              'z_score_volcano': z_score_volcano['score']
-            }
-        context['results'] = results_dict
-        context['n_results'] = len(results_dict.keys())
+                # just in case the it was empty
+                if (('fold_change' in ddf.columns) and (ddf.shape[0]>0)):
+                    z_score = phosphoproteomics_script.KSEA(ddf) # for all
+                else:
+                    z_score =  {'non_identified':[],'score':[]}
+                    # just for the ones that are above threshold in volcano plot
+                    # cause of the thresholds, sometimes there could be empty dataframes
+                if (('fold_change' in df_volcano.columns) and (df_volcano.shape[0] > 0)):
+                    z_score_volcano = phosphoproteomics_script.KSEA(df_volcano)
+                else:
+                    z_score_volcano = {'non_identified':[],'score':[]}
 
-#        except:
-#            os.remove(tmp_file_path)
-#            return 'Impossible to calculate, something wrong in the input values. <a href="/"> Go back </a>'
+                non_identified = z_score['non_identified'] if z_score['non_identified'] else 0
+                non_identified_volcano =  z_score_volcano['non_identified'] if z_score_volcano['non_identified'] else 0
+                results_dict[inhibitor] = {
+                    'volcano': results_volcano, 'fold_threshold': fold_threshold,
+                    'pval_threshold': pval_threshold,
+                    'non_identified': non_identified,
+                    'z_score': z_score['score'],
+                    'non_identified_volcano': non_identified_volcano,
+                    'z_score_volcano': z_score_volcano['score']
+                    }
+                context['results'] = results_dict
+                context['n_results'] = len(results_dict.keys())
 
-
-    return render_template('phosphoproteomics.html', context = context)
+            return render_template('phosphoproteomics.html', context = context)
+        except:
+            return 'Something went wrong during data analysis, please check that the file has the right format and column names <a href="/"> Go back </a>'
